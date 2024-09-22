2b57341477a0c3218d8708f3e0b8354583be0b21
Andrew Skow <skovorodnikov11@gmail.com>
2024-09-23 00:17:36 +0300

    hw04_done

---
+++ Lesson04.java
@@ -0,0 +1,7 @@
+public class Lesson04 {
+    public static void main(String[] args) {
+        int age = 4;
+        if (age > 18) {
+            System.out.println("Ты должен праздновать");
+            if (age < 21) {
+                Sy
