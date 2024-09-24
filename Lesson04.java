210968f9331e93490f860cdfaac1038665397c62
Andrew Skow <skovorodnikov11@gmail.com>
2024-09-24 23:50:26 +0300

    hw05

--- Lesson04.java
+++ Lesson04.java
@@ -4,4 +4,47 @@ public class Lesson04 {
         if (age > 18) {
             System.out.println("Ты должен праздновать");
             if (age < 21) {
-                Sy
+                System.out.println("Выпей сливочного пива!");
+            } else {
+                System.out.println("Иди куда хочешь и пей, что хочешь!");
+            }
+        } else {
+            if (age > 7) System.out.println("Иди в школу");
+            else System.out.println("Иди в детский сад");
+        }
+
+        char color = 'g';
+
+        if (color == 'r') {
+            System.out.println("Красный, ехать нельзя!");
+        } else {
+            if (color == 'y') {
+                System.out.println("Жёлтый! Внимание!");
+            } else {
+                System.out.println("Зелёный! Проезд разрешён!");
+            }
+        }
+
+        System.out.println("Использование оператора switch");
+        color = 'y';
+
+
+        switch (color) {
+            case 'r':
+                System.out.println("Красный, ехать нельзя!");
+                break;
+            case 'y':
+                System.out.println("Жёлтый! Внимание!");
+                break;
+            case 'g':
+                System.out.println("Зелёный! Проезд разрешён!");
+                break;
+            case 'l':
+                System.out.println("Поворот налево!");
+                break;
+            default:
+                System.out.println("Светофор сломался");
+        }
+
+    }
+}
